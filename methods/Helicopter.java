1af3d17270eb159bbb9e8e935f75802fe96efb56
bhagya hebbal <hebbalbhagya304@gmail.com>
2024-07-31 00:08:08 +0530

    new files

---
+++ methods/Helicopter.java
@@ -0,0 +1,10 @@
+//1st task 23-7-24
+
+
+class Helicopter
+{
+	Helicopter()
+	{
+	    System.out.println("Create Helicopter");
+	}
+}
---
+++ methods/HelicopterRunner.java
@@ -0,0 +1,32 @@
+//1st task 23-7-24
+
+
+class HelicopterRunner
+{
+	public static void main(String[] values)
+	{
+		System.out.println("Start main in HelicopterRunner");
+		
+		Helicopter helicopter1=new Helicopter();
+		Helicopter helicopter2=new Helicopter();
+		Helicopter helicopter3=new Helicopter();
+		Helicopter helicopter4=new Helicopter();
+		Helicopter helicopter5=new Helicopter();
+		
+		Market market1=new Market();
+		Market market2=new Market();
+		Market market3=new Market();
+		Market market4=new Market();
+		Market market5=new Market();
+		
+		Mall mall=new Mall();
+		Mall mall2=new Mall();
+		Mall mall3=new Mall();
+		Mall mall4=new Mall();
+		Mall mall5=new Mall();
+		
+		
+		
+		System.out.println("Start main in HelicopterRunner");
+	}
+}
